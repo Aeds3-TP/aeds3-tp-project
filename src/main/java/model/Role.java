d87cb8514c184817f06e952030ed260fe8739acb
Gabriel Assis <gabrielassisclacerda@gmail.com>
2026-03-23 15:02:25 -0300

    OrdenacaoExterna adicionado mas não implementado

--- src/main/java/model/Role.java
+++ src/main/java/model/Role.java
@@ -1,7 +1,8 @@
 package model;
+import java.io.Serializable;
 
 //é apenas um enum (tipo uma classe) pra obrigar que o usuario tenha uma role dessas
-public enum Role {
+public enum Role implements Serializable{
     ADMIN,      // Manda em tudo
     GESTOR,     // Acesso intermediário
     USUARIO;    // Acesso básico
